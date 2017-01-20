cbcda6301f25794f8b90bee05768035cf213253f
Tobrun Van Nuland <tobrun.van.nuland@gmail.com>
2017-01-20 13:34:47 +0100

    [android] - zoom to at least 2 when tracking is enabled, introduce interface for zoom invalidation, simplify example

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/maps/TrackingSettings.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/maps/TrackingSettings.java
@@ -2,6 +2,7 @@ package com.mapbox.mapboxsdk.maps;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Camera;
 import android.graphics.PointF;
 import android.location.Location;
 import android.os.Bundle;
@@ -28,6 +29,7 @@ public final class TrackingSettings {
   private final MyLocationView myLocationView;
   private final UiSettings uiSettings;
   private final FocalPointChangeListener focalPointChangedListener;
+  private final CameraZoomInvalidator zoomInvalidator;
   private LocationListener myLocationListener;
 
   private boolean myLocationEnabled;
@@ -38,10 +40,11 @@ public final class TrackingSettings {
   private MapboxMap.OnMyBearingTrackingModeChangeListener onMyBearingTrackingModeChangeListener;
 
   TrackingSettings(@NonNull MyLocationView myLocationView, UiSettings uiSettings,
-                   FocalPointChangeListener focalPointChangedListener) {
+                   FocalPointChangeListener focalPointChangedListener, CameraZoomInvalidator zoomInvalidator) {
     this.myLocationView = myLocationView;
     this.focalPointChangedListener = focalPointChangedListener;
     this.uiSettings = uiSettings;
+    this.zoomInvalidator = zoomInvalidator;
   }
 
   void initialise(MapboxMapOptions options) {
@@ -92,6 +95,7 @@ public final class TrackingSettings {
     myLocationView.setMyLocationTrackingMode(myLocationTrackingMode);
 
     if (myLocationTrackingMode == MyLocationTracking.TRACKING_FOLLOW) {
+      zoomInvalidator.zoomTo(2.0);
       focalPointChangedListener.onFocalPointChanged(new PointF(myLocationView.getCenterX(),
         myLocationView.getCenterY()));
     } else {
@@ -342,5 +346,9 @@ public final class TrackingSettings {
 
   void onStop() {
     myLocationView.onStop();
+  }
+
+  interface CameraZoomInvalidator {
+    void zoomTo(double zoomLevel);
   }
 }
